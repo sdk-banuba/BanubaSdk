c7e436fe20e3d9833027a26154c76a1f452b8da8
ci <ci@banuba.com>
2024-12-06 20:11:10 +0300

    1.14.1-199-g2f4e3cf953

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-219-gefae079ac3"
+let bnbPackageVersion: Version = "1.14.1-199-g2f4e3cf953"
 
 let package = Package(
     name: "BanubaSdk",
@@ -96,8 +96,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BanubaSdk",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-219-gefae079ac3/BanubaSdk.zip",
-            checksum: "0bce7200508e8c11276c14d6879ce8f0e399e48bf3932ab7184aec45cf4af123"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-199-g2f4e3cf953/BanubaSdk.zip",
+            checksum: "d96d8a98d6324bc7dce26e418fe02f23c6fb798745480a713d034a73695ab620"
         ),
         .target(
             name: "BanubaSdk_BNBSdkCore",
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 #### About packages see [HERE](https://docs.banuba.com/face-ar-sdk-v1/ios/ios_packages)
 
-#### Package version: **1.14.1-219-gefae079ac3**
+#### Package version: **1.14.1-199-g2f4e3cf953**
 
 #### **[Banuba homepage](https://banuba.com)**
 
